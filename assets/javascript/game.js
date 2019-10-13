384c64ffa6591a777fb6e3f4ba4e3af0a6d14f13
Megan Lockette <megan3693@sbcglobal.net>
2019-10-13 13:22:46 -0700

    editing javascript and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,35 @@
-document.addEventListener("keydown", function(event) {
-    var userGuess = event.key;
+var options = ["a", "b", "c", "e", "g", "i", "k", "l", "m", "o", "t", "u", "v", "w", "x", "y", "z"];
+var guesses = 10
+var lettersGuessed = 0;
+var win = 0;
+var loss = 0;
+// Not sure how to apply these classes to the html
+var guessesLeft = document.querySelector(".guessesLeft")
+    guessesLeft.innerHTML = "Guesses Left: " + guesses
+document.onkeyup = function () {
+    var userguess = String.fromCharCode(event.keyCode).
+        toLowerCase();
 
-    if (userGuess === randomLetter){
-        console.log ("correctGuess");
-    } 
+    console.log(userguess);
+
+    var computerGuess = options[Math.floor(Math.random() * options
+        .length)];
+
+    console.log(computerGuess);
+
+
+
+if (userguess == computerGuess){
+
+    win.innerHTML = "Wins: "
+    console.log("You won!")
+
+} else {
+// Need to stop the user from continuing guesses after 10 guesses have been made.
+    guesses-- 
+    guessesLeft.innerHTML = "Guesses Left: " + guesses
+    console.log(guesses)
+    console.log("You lost!")
+}
+}
+
