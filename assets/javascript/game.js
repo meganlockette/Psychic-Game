97cb763c2696939cf5b7ae351a1fb6a3d53b2d9e
Megan Lockette <megan3693@sbcglobal.net>
2019-10-13 21:49:28 -0700

    adding css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,14 @@ var lettersGuessed = 0;
 var win = 0;
 var loss = 0;
 // Not sure how to apply these classes to the html
+var win = document.querySelector(".win")
+    win.innerHTML = "Wins: " //variable needs to be completed
+var loss = document.querySelector(".loss")
+    loss.innerHTML = "Losses: " //variable needs to be completetd
 var guessesLeft = document.querySelector(".guessesLeft")
     guessesLeft.innerHTML = "Guesses Left: " + guesses
+var lettersGuessed = document.querySelector(".lettersGuessed")
+    lettersGuessed.innerHTML = "Guesses so far: " //variable needs to be completed
 document.onkeyup = function () {
     var userguess = String.fromCharCode(event.keyCode).
         toLowerCase();
@@ -20,8 +26,7 @@ document.onkeyup = function () {
 
 
 if (userguess == computerGuess){
-
-    win.innerHTML = "Wins: "
+    
     console.log("You won!")
 
 } else {
